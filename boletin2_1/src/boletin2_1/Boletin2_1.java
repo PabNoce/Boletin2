c35ba916c1e029410400a3eda3f89038c4303b9b
pnocedalopez <pnocedalopez@victoria15.danielcastelao.org>
2017-10-26 09:39:52 +0200

    E2

---
+++ boletin2_1/src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_1;
+
+/**
+ *
+ * @author pnocedalopez
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  float area, base=4, altura=3;
+  area=base*altura/2;
+        System.out.println("El area del triangulo es "+area);
+    }
+    
+}
